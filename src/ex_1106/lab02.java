7817502bb77d9f1cb2fa09de1e266e24467b2277
Niharika <niha1730@gmail.com>
2024-06-17 06:52:18 -0700

    BODMAS concatination

---
+++ src/ex_1106/lab02.java
@@ -0,0 +1,13 @@
+package ex_1106;
+
+public class lab02 {
+    public static void main(String[] args) {
+        int a = 80;
+        long b = 90;
+        String s1 = "KK";
+        String s2 = "VV";
+        System.out.println(s1+s2+a+b);
+        System.out.println(s1+s2+(a+b));//BODMAS- Bracets of div,multi, add, sub
+        //when we add bracets comcatination happens
+    }
+}
